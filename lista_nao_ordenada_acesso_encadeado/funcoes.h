13a30257b1a7247d1ae331adf3ed3a7c548710f1
Victor Hugo Marra Araujo <31578843+vhmarra@users.noreply.github.com>
2019-04-21 18:42:49 -0300

    Add files via upload

---
+++ lista_nao_ordenada_acesso_encadeado/funcoes.h
@@ -0,0 +1,5 @@
+typedef struct noh *Lista;
+cria_lista();
+int lista_vazia(Lista *lst);
+int insere_elem(Lista *lst, int elem);
+int remove_elemento(Lista *lst, int elem);
---
+++ lista_nao_ordenada_acesso_encadeado/usuario.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+cria_lista();
+
+
+
+
+  return 0;
+}
