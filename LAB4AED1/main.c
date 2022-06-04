f7ef0391855f6d66e37c27bb926b86781daae86b
Victor Hugo Marra Araujo <marravh@gmail.com>
2022-06-04 20:00:32 -0300

    Add files via upload

--- LAB4AED1/main.c
+++ LAB4AED1/main.c
@@ -4,7 +4,14 @@
 int main() {
     pt *p;
     p = generate();
-    int veri = set_pto(p,13.0,1.0,1.0,1.0);
+    double dista[4];
+
+    for (int i = 0; i < 4 ; ++i) {
+        printf("Entre com D%d: ",i+1);
+        scanf("%lf",&dista[i]);
+    }
+
+    int veri = set_pto(p,dista[0],dista[1],dista[2],dista[3]);
     if(veri) {
         printf("Pontos setados sucesso\n");
     } else {
@@ -13,7 +20,7 @@ int main() {
     }
 
     double distancia = dist(p);
-    printf("%lf",distancia);
+    printf("distancia -> %lf",distancia);
     freeP(p);
 
     return 0;
